b5314efaa70dd2cc6698003ae9288ef6d92a1c52
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-03-26 17:22:25 +0200

    First two exercises from LeetCode 75

---
+++ LeetCode 75/1480. Running Sum of 1d Array/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public int[] RunningSum(int[] nums) {
+        // Iterate through the array starting from the second element
+        for (int i = 1; i < nums.Length; i++) {
+            // Add the previous element to the current element
+            nums[i] += nums[i-1];
+        }
+        // Return the modified array
+        return nums;
+    }
+}
---
+++ LeetCode 75/724. Find Pivot Index/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace DefaultNamespace;
+
+public class Solution
+{
+    public class Solution {
+        public int PivotIndex(int[] nums) {
+            // Calculate the sum of all elements in the array
+            int sum = 0;
+            foreach (int num in nums) {
+                sum += num;
+            }
+        
+            // Initialize the running sum of elements to the left
+            int leftSum = 0;
+            // Iterate through the array to find the pivot index
+            for (int i = 0; i < nums.Length; i++) {
+                // Check if the left sum is equal to the right sum
+                if (leftSum == sum - leftSum - nums[i]) {
+                    return i;
+                }
+                // Add the current element to the left sum
+                leftSum += nums[i];
+            }
+        
+            // No pivot index found, return -1
+            return -1;
+        }
+    }
+
+}
