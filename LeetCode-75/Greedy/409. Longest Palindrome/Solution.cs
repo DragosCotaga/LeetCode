dc700a638a3e101b297aac15bf3143a121e0000d
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-03-30 15:18:18 +0200

    Day 5 - Greedy

---
+++ LeetCode-75/Greedy/409. Longest Palindrome/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public int LongestPalindrome(string s) {
+        HashSet<char> set = new HashSet<char>();
+        int longestPalindrome = 0;
+        
+        foreach (char c in s) {
+            if (set.Contains(c)) {
+                set.Remove(c);
+                longestPalindrome += 2;
+            } else {
+                set.Add(c);
+            }
+        }
+        
+        if (set.Count > 0) {
+            longestPalindrome += 1;
+        }
+        
+        return longestPalindrome;
+    }
+}
