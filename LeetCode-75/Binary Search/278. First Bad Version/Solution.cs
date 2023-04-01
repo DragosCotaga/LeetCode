2fc153e300f71cb35c5e42d26ae5a202a20faa07
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-04-01 09:55:29 +0200

    Day 7 - Binary Search

---
+++ LeetCode-75/Binary Search/278. First Bad Version/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace DefaultNamespace;
+
+public class Solution : VersionControl {
+    public int FirstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int middle = left + (right - left) / 2;
+            if (IsBadVersion(middle)) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return left;
+    }
+}
---
+++ LeetCode-75/Binary Search/704. Binary Search/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length - 1;
+        while (left <= right) {
+            int middle = (left + right) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            } else if (target < nums[middle]) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return -1;
+    }
+}
