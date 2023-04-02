0171375398ecf62590c4ded5ff10d70da23f48fb
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-04-02 22:09:23 +0200

    Day 8 - Binary Search Tree

---
+++ LeetCode-75/Binary Search Tree/98. Validate Binary Search Tree/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace DefaultNamespace;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsValidBST(TreeNode root) {
+        return IsValidBSTHelper(root, null, null);
+    }
+
+    private bool IsValidBSTHelper(TreeNode node, int? minVal, int? maxVal) {
+        if (node == null) return true;
+        if ((minVal != null && node.val <= minVal) || (maxVal != null && node.val >= maxVal)) {
+            return false;
+        }
+        return IsValidBSTHelper(node.left, minVal, node.val) && IsValidBSTHelper(node.right, node.val, maxVal);
+    }
+}
