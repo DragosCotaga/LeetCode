3d375459c4589fee0bf792898dc1e72b49f24356
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-04-03 09:40:06 +0200

    Day 9 - Graph/BFS/DFS

---
+++ LeetCode-75/Graph-BFS-DFS/200. Number of Islands/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public int NumIslands(char[][] grid) {
+        int islandCount = 0;
+        for (int i = 0; i < grid.Length; i++) {
+            for (int j = 0; j < grid[0].Length; j++) {
+                if (grid[i][j] == '1') {
+                    islandCount++;
+                    MarkConnectedCells(grid, i, j);
+                }
+            }
+        }
+        return islandCount;
+    }
+
+    private void MarkConnectedCells(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.Length || j < 0 || j >= grid[0].Length || grid[i][j] == '0') {
+            return;
+        }
+        grid[i][j] = '0';
+        MarkConnectedCells(grid, i + 1, j);
+        MarkConnectedCells(grid, i - 1, j);
+        MarkConnectedCells(grid, i, j + 1);
+        MarkConnectedCells(grid, i, j - 1);
+    }
+
+}
---
+++ LeetCode-75/Graph-BFS-DFS/733. Flood Fill/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace DefaultNamespace;
+
+public class Solution
+{
+    public int[][] FloodFill(int[][] image, int sr, int sc, int newColor) {
+        if (image[sr][sc] == newColor) return image;
+        FloodFillHelper(image, sr, sc, image[sr][sc], newColor);
+        return image;
+    }
+
+    private void FloodFillHelper(int[][] image, int i, int j, int oldColor, int newColor) {
+        if (i < 0 || i >= image.Length || j < 0 || j >= image[0].Length || image[i][j] != oldColor) {
+            return;
+        }
+        image[i][j] = newColor;
+        FloodFillHelper(image, i + 1, j, oldColor, newColor);
+        FloodFillHelper(image, i - 1, j, oldColor, newColor);
+        FloodFillHelper(image, i, j + 1, oldColor, newColor);
+        FloodFillHelper(image, i, j - 1, oldColor, newColor);
+    }
+
+}
