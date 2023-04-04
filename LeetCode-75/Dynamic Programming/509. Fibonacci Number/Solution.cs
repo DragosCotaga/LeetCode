409327a55c2928ffec798524d8665ae2d5b8de01
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-04-04 18:54:52 +0200

    Day 10 - Dynamic Programming

---
+++ LeetCode-75/Dynamic Programming/509. Fibonacci Number/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public int Fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        
+        int[] fib = new int[n + 1];
+        fib[0] = 0;
+        fib[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        
+        return fib[n];
+    }
+}
