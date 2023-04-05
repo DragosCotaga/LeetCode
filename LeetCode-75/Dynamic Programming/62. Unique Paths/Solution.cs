78ab1c1fc5acb86faeb1b2d844e94874dbfb94d8
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-04-05 12:54:06 +0200

    Day 11 - Dynamic Programming

---
+++ LeetCode-75/Dynamic Programming/62. Unique Paths/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public int UniquePaths(int m, int n) {
+        int[,] dp = new int[m, n];
+        
+        // Initialize the first row and first column to 1
+        for (int i = 0; i < m; i++) {
+            dp[i, 0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0, j] = 1;
+        }
+        
+        // Calculate the number of unique paths for each cell in the grid
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i, j] = dp[i - 1, j] + dp[i, j - 1];
+            }
+        }
+        
+        // Return the number of unique paths to reach the bottom-right corner
+        return dp[m - 1, n - 1];
+    }
+}
