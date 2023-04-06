723cc7d8eced1416c11c6889533b71bb75e06e4a
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-04-06 21:37:38 +0200

    Day 12 - Sliding Window/Two Pointer

---
+++ LeetCode-75/Sliding Window-Two Pointer/424. Longest Repeating Character Replacement/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public int CharacterReplacement(string s, int k) {
+        int[] count = new int[26];
+        int maxCount = 0;
+        int maxLength = 0;
+        int start = 0;
+        
+        for(int end = 0; end < s.Length; end++) {
+            count[s[end] - 'A']++;
+            maxCount = Math.Max(maxCount, count[s[end] - 'A']);
+            
+            if(end - start + 1 - maxCount > k) {
+                count[s[start] - 'A']--;
+                start++;
+            }
+            
+            maxLength = Math.Max(maxLength, end - start + 1);
+        }
+        
+        return maxLength;
+    }
+}
---
+++ LeetCode-75/Sliding Window-Two Pointer/438. Find All Anagrams in a String/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public IList<int> FindAnagrams(string s, string p) {
+        IList<int> result = new List<int>();
+        int[] freqP = new int[26];
+        int[] freqS = new int[26];
+        int lenP = p.Length;
+        int lenS = s.Length;
+        
+        // count the frequency of characters in p
+        foreach(char c in p) {
+            freqP[c - 'a']++;
+        }
+        
+        // sliding window
+        for(int i = 0; i < lenS; i++) {
+            // add current character to frequency array
+            freqS[s[i] - 'a']++;
+            
+            // remove first character from frequency array
+            if(i >= lenP) {
+                freqS[s[i-lenP] - 'a']--;
+            }
+            
+            // check if frequency arrays are equal
+            if(i >= lenP - 1 && IsEqual(freqS, freqP)) {
+                result.Add(i - lenP + 1);
+            }
+        }
+        
+        return result;
+    }
+    
+    private bool IsEqual(int[] freqS, int[] freqP) {
+        for(int i = 0; i < 26; i++) {
+            if(freqS[i] != freqP[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
