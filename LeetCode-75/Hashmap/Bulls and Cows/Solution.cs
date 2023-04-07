b6a965e1cba154af5cc7af2c1b8dfb730680c474
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-04-07 10:09:26 +0200

    Day 13 - Hashmap

---
+++ LeetCode-75/Hashmap/Bulls and Cows/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public string GetHint(string secret, string guess) {
+        int bulls = 0;
+        int cows = 0;
+        int[] freq = new int[10];
+        
+        for(int i = 0; i < secret.Length; i++) {
+            if(secret[i] == guess[i]) {
+                bulls++;
+            } else {
+                freq[secret[i] - '0']++;
+            }
+        }
+        
+        for(int i = 0; i < guess.Length; i++) {
+            if(secret[i] != guess[i] && freq[guess[i] - '0'] > 0) {
+                cows++;
+                freq[guess[i] - '0']--;
+            }
+        }
+        
+        return $"{bulls}A{cows}B";
+    }
+}
