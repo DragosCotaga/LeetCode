0f624e9d63f1d05e316157e25299ef6dd25b3158
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-04-08 21:07:14 +0200

    Day 14 - Stack

---
+++ LeetCode-75/Stack/394. Decode String/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public string DecodeString(string s) {
+        var stack = new Stack<(int, string)>();
+        int repeatCount = 0;
+        string decodedString = "";
+
+        foreach (char c in s) {
+            if (Char.IsDigit(c)) {
+                repeatCount = repeatCount * 10 + (c - '0');
+            } else if (Char.IsLetter(c)) {
+                decodedString += c;
+            } else if (c == '[') {
+                stack.Push((repeatCount, decodedString));
+                repeatCount = 0;
+                decodedString = "";
+            } else if (c == ']') {
+                (int prevRepeatCount, string prevDecodedString) = stack.Pop();
+                decodedString = prevDecodedString + String.Concat(Enumerable.Repeat(decodedString, prevRepeatCount));
+            }
+        }
+
+        return decodedString;
+    }
+
+}
---
+++ LeetCode-75/Stack/844. Backspace String Compare/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace DefaultNamespace;
+
+public class Solution {
+    public bool BackspaceCompare(string s, string t) {
+        var sStack = new Stack<char>();
+        var tStack = new Stack<char>();
+
+        foreach (char c in s) {
+            if (c == '#' && sStack.Count > 0) {
+                sStack.Pop();
+            } else if (c != '#') {
+                sStack.Push(c);
+            }
+        }
+
+        foreach (char c in t) {
+            if (c == '#' && tStack.Count > 0) {
+                tStack.Pop();
+            } else if (c != '#') {
+                tStack.Push(c);
+            }
+        }
+
+        return sStack.SequenceEqual(tStack);
+    }
+
+}
