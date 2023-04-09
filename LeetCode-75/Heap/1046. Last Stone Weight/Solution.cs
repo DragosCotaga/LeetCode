5f1ec88020f0c2deaa2d5a3a67fd36d80e8c262a
DragosCotaga <60718498+DragosCotaga@users.noreply.github.com>
2023-04-09 20:22:12 +0200

    Finished first 15 days of LeetCode Blind 75 Day 15 - Heap

---
+++ LeetCode-75/Heap/1046. Last Stone Weight/Solution.cs
@@ -0,0 +1,24 @@
+﻿import java.util.PriorityQueue;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        // Create a max heap to store the weights of stones
+        PriorityQueue<Integer> heap = new PriorityQueue<>((x, y) -> y - x);
+        for (int stone : stones) {
+            heap.offer(stone);
+        }
+        
+        // Smash stones until there is at most one left in the heap
+        while (heap.size() > 1) {
+            int y = heap.poll();
+            int x = heap.poll();
+            if (x != y) {
+                heap.offer(y - x);
+            }
+        }
+        
+        // Return the weight of the last remaining stone, or 0 if there are none
+        return heap.isEmpty() ? 0 : heap.peek();
+    }
+}
+
---
+++ LeetCode-75/Heap/692. Top K Frequent Words/Solution.cs
@@ -0,0 +1,36 @@
+﻿class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        // Create a hashmap to count the frequency of each word
+        Map<String, Integer> count = new HashMap<String, Integer>();
+        for (String word : words) {
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        }
+        
+        // Create a priority queue to store the top k frequent words
+        PriorityQueue<String> heap = new PriorityQueue<String>(new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+            int freq1 = count.get(a);
+            int freq2 = count.get(b);
+            if (freq1 != freq2) {
+            return freq2 - freq1;
+        } else {
+            return a.compareTo(b);
+        }
+        }
+        });
+        
+        // Add each word to the priority queue
+        for (String word : count.keySet()) {
+            heap.offer(word);
+        }
+        
+        // Build the result list
+        List<String> result = new ArrayList<String>();
+        for (int i = 0; i < k; i++) {
+            result.add(heap.poll());
+        }
+        
+        return result;
+    }
+}
